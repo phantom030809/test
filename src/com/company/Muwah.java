06624644dda7369166c5bb0ef4e7dc81efc159c7
McuBlaster <sn026803310@gmail.com>
2020-05-03 21:55:00 +0300

    Meet & Eat Mehow!

---
+++ src/com/company/Muwah.java
@@ -0,0 +1,33 @@
+package com.company;
+import java.util.Date;
+
+public class Muwah {
+    private int age;
+    private Date data_of_birth;
+    private String address;
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getData_of_birth() {
+        return data_of_birth;
+    }
+
+    public void setData_of_birth(Date data_of_birth) {
+        this.data_of_birth = data_of_birth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
